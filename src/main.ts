b2e275443d67feea37015f0ec80fbddde706a505
Petar Petrovic <p3ki99@gmail.com>
2024-08-14 02:59:41 +0200

    setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ConfigService } from "@nestjs/config";
+import { SwaggerModule, DocumentBuilder, SwaggerDocumentOptions } from "@nestjs/swagger";
+import { INestApplication } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -10,6 +12,24 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const port = configService.get<number>("port", 3000);
 
+  configureSwagger(app);
+
   await app.listen(port);
 }
 bootstrap();
+
+function configureSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle("Battle Tanks")
+    .setDescription("The Battle Tanks API description")
+    .setVersion("1.0")
+    .addTag("battle-tanks")
+    .build();
+
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (_controllerKey: string, methodKey: string) => methodKey,
+  };
+
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup("api", app, document);
+}
